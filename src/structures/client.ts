88953727793292acf4647dc0a6f695cf021fd17f
Shiv <fangyt.gaming@gmail.com>
2022-05-03 18:56:06 +0530

    refactor: change api dir

--- src/structures/client.ts
+++ src/structures/client.ts
@@ -2,7 +2,7 @@ import { REST } from "@discordjs/rest";
 import { Routes } from "discord-api-types/v9";
 
 import { Client, ClientOptions, CommandInteraction } from "discord.js";
-import startApi from "./api";
+import startApi from "../api";
 import Command from "./command";
 
 export default class Radium extends Client {
